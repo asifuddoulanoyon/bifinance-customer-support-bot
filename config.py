07db98904a6ea2c15185e9e39d0fba33186f9f42
asifuddoulanoyon <105268068+asifuddoulanoyon@users.noreply.github.com>
2026-01-31 18:23:44 +0600

    Change BOT_OWNER_ID and BOT_TOKEN values
    Updated bot owner ID and token in config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 # Owner Telegram ID (replace with your Telegram ID)
-BOT_OWNER_ID = 123456789
+BOT_OWNER_ID = 1675295056
 
 # Bot Token from @BotFather
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("8558141334:AAHZ4ZbLVTufnlKU4pPKBIdKf5fgI-EBI7o")
 
 # Agents: Telegram IDs, empty initially
 AGENTS = []
